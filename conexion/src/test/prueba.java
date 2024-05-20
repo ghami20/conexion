d94a2e1cca7447d32afdceb93716c9e80a34a55c
ghami20 <ghamiboxbox@gmail.com>
2024-05-20 15:52:23 -0300

    Cambios

--- conexion/src/test/prueba.java
+++ conexion/src/test/prueba.java
@@ -2,12 +2,14 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import controlador.UsuarioControlador;
 import Modelo.Usuario;
 public class prueba {
 
 	@Test
+	@DisplayName("Prueba de iniciar sesiòn con valores correcots")
 	public void PruebaIniciarSesionV() {
 		UsuarioControlador controlador = new UsuarioControlador();
 		boolean flag=false;
@@ -18,19 +20,20 @@ public class prueba {
 			} 
 		}
 		assertEquals(true,flag);
-
 	}
 	@Test
+	@DisplayName("Prueba de iniciar sesiòn con valores incorrectos")
 	public void PruebaIniciarSesionF() {
 		UsuarioControlador controlador = new UsuarioControlador();
 		boolean flag=false;
 		for (Usuario usuario : controlador.getAllUsers()) {
-			if (usuario.IniciarSesion("Gamaliel", "Ghami@gmail.com")) {
+			if (usuario.IniciarSesion("sadas", "Ghami@gmail.com")) {
 				flag =true;
 				break;
 			} 
 		}
-		assertEquals(true,flag);
+		assertEquals(false,flag);
 
 	}
+	
 }
